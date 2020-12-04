56603b754fd320f81537b932e0a7c1cf6ae56969
Dawid Kubicki <dawid@pluscode.io>
2020-12-04 20:05:03 +0100

    minor changes

--- WatchOSTimer WatchKit Extension/ContentView.swift
+++ WatchOSTimer WatchKit Extension/ContentView.swift
@@ -8,9 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var secondScreenShown = false
+    @State var timerVal = 5
+    
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+        VStack {
+            Text("Start timer for: \(timerVal)")
+            Picker(selection: $timerVal, label: Text("Picker")) {
+                Text("1").tag(1)
+                Text("5").tag(5)
+                Text("10").tag(10)
+                Text("15").tag(15)
+            }
+            NavigationLink(
+                destination: SecondView(secondScreenShown: $secondScreenShown, timerVal: timerVal),
+                isActive: $secondScreenShown,
+                label: {
+                    Text("Navigate")
+                })
+        }
+    }
+}
+
+
+struct SecondView: View {
+    
+    @Binding var secondScreenShown: Bool
+    @State var timerVal = 1
+    
+    var body: some View {
+        VStack{
+            Text("Time remaining")
+            Text("\(timerVal)")
+            Button(action: {self.secondScreenShown = false}, label: {
+                Text("Cancel")
+            })
+        }
     }
 }
 
